f11ed855ec64d52e0c469839b720811c667f9a8f
Mohamed Maher <139143509+mohamedmaher-dev@users.noreply.github.com>
2023-12-07 15:00:18 -0800

    v1

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,6 @@ import 'firebase_options.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-
   runApp(
     MultiBlocProvider(
       providers: [
